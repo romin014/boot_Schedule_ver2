2d97d83eede1325eb95657ead3f17530940fe9a4
romin014 <sohyun014@naver.com>
2025-04-03 21:07:15 +0900

    init

---
+++ src/main/java/com/example/boot_schedule_ver2/BootScheduleVer2Application.java
@@ -0,0 +1,13 @@
+package com.example.boot_schedule_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootScheduleVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootScheduleVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boot_schedule_ver2/BootScheduleVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot_schedule_ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootScheduleVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
